f7691293c02456881be1c8bebacf07e707f0e9c5
Daniel Ramon <danielramoon@gmail.com>
2024-07-14 10:30:54 -0300

    init-vue-header-banner

---
+++ vue/src/components/BannerComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="banner">
+    <img class="banner-image" :src="bannerImage" alt="Banner Image" />
+    <h1 class="banner-title">Seja bem-vindo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent",
+  data() {
+    return {
+      bannerImage: require("@/assets/images/banner.jpeg"),
+    };
+  },
+};
+</script>
+
+<style scoped src="@/styles/banner.css"></style>
---
+++ vue/src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <img src="@/assets/images/logo.png" alt="TruckMe Logo" />
+    </div>
+    <nav class="nav-menu">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">Sobre</router-link></li>
+        <li><router-link to="/services">Serviços</router-link></li>
+        <li><router-link to="/contact">Contato</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped src="@/styles/header.css"></style>
